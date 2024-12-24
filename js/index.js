4244aae641f5728d13b5a0df4d0f5ca49ec21c19
Yaroslav <lgalahad88@gmail.com>
2024-12-24 20:00:44 +0200

    2

--- js/index.js
+++ js/index.js
@@ -1,32 +1,31 @@
 'use strict';
 
+const userStr = prompt("введіть рядок");
+let userArrChar = prompt("введіть символ(и), для видалення із рядка");
 
-function getStrCharRemove(dataStr, arrCharRemove) {
+if (!userStr?.trim() || !userArrChar?.trim()) {
+    alert("помилка: введіть рядок та мінімум один символ");
+}
+else {
+    userArrChar = userArrChar.split("").map(char => char.trim());
+    const userResult = getStrCharRemove(userStr, userArrChar);
 
-    if (typeof dataStr !== "string" || !dataStr.trim()) {
-        return "Пустий рядок, впишіть довільний рядок в лапках";
-    }
-    else if (!Array.isArray(arrCharRemove) || arrCharRemove.length === 0) {
-        return "Потрібен хоча б один символ для видалення з рядка, задайте символи в масиві";
-    }
-    else {
-        let result = dataStr.split("");
-        let i = 0;
-
-        while(i < result.length) {
-            if(arrCharRemove.includes(result[i])) {
-                result.splice(i, 1);
-            }
-            else {
-                i++;
-            }
-        }  
-        
-        return result.join("");
-    }
-    
+    alert(userResult);
 }
 
-console.log(getStrCharRemove("hello world", ["l", "d"]));
+function getStrCharRemove(dataStr, arrCharRemove) {
+    
+    let result = dataStr.split("");
+    let i = 0;
 
-
+    while(i < result.length) {
+        if(arrCharRemove.includes(result[i])) {
+            result.splice(i, 1);
+        }
+        else {
+            i++;
+        }
+    }  
+    
+    return result.join("");
+}
